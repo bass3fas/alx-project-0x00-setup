680b36c13310a93efa1ed74f74b8410b99e475b1
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-17 12:24:26 +0400

    6. Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,13 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 const Landing: React.FC =  () => {
     return (
       <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Card />
+        <Button title="small" styles="border bg-gray-500 text-white px-2 py-1 text-sm rounded-sm" />
+        <Button title="medium" styles="border bg-gray-500 text-white px-4 py-2 text-md rounded-md" />
+        <Button title="large" styles="border bg-gray-500 text-white px-6 py-3 text-lg rounded-full" />
       </div>
     )
   }
